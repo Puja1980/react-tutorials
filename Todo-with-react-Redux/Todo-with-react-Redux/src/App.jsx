9bc0aae1d57c1a2a1100dc29c6f48bbfef2e8349
puja1980 <pujaadhikari554@gmail.com>
2024-06-20 16:24:26 +0530

    new project of React-Redux

---
+++ Todo-with-react-Redux/Todo-with-react-Redux/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='flex justify-center items-center bg-slate-500 text-white'>Hellow World</h1>
+    </>
+  )
+}
+
+export default App
--- Todo-with-reactVite/src/components/SingleTodo.jsx
+++ Todo-with-reactVite/src/components/SingleTodo.jsx
@@ -26,25 +26,31 @@ function SingleTodo({ todo }) {
         type="text"
         value={todoMsg}
         onChange={(e) => setTodoMsg(e.target.value)}
-        className={`bg-transparent outline-none text-white w-3/4 ${isTodoEditable? 'px-1 bg-blue-600 rounded-xl border-1 border-black':''} ${todo.completed ? 'line-through':''}`}
+        className={`bg-transparent outline-none text-white w-3/4 ${
+          isTodoEditable
+            ? "px-1 bg-blue-700 rounded-xl border-1 border-black"
+            : ""
+        } ${todo.completed ? "line-through" : ""}`}
       />
 
       <div className="flex">
         <button
           onClick={() => {
-            if(todo.completed) return;
-            if(isTodoEditable){
-              editTodo()
-            }else setIsTodoEditable((prev) => !prev);
+            if (todo.completed) return;
+            if (isTodoEditable) {
+              editTodo();
+            } else setIsTodoEditable((prev) => !prev);
           }}
-          className={`bg-green-500 text-white mr-1  px-3 rounded-lg ${isTodoEditable? 'bg-yellow-500 text-black': ''}`}
+          className={`bg-green-500 text-white mr-1  px-3 rounded-lg ${
+            isTodoEditable ? "bg-yellow-500 text-black" : ""
+          }`}
         >
-          {isTodoEditable ? 'Save': 'Edit'}
+          {isTodoEditable ? "Save" : "Edit"}
         </button>
 
         <button
           onClick={() => removeTodo(todo.id)}
-          disabled = {todo.completed}
+          disabled={todo.completed}
           className="bg-red-500 text-white px-3 rounded-lg"
         >
           X
